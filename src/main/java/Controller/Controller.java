a8666275df7e2d2a9f4de3642569a1125d824ec5
Roman Bulinski <aboandrus@gmail.com>
2019-04-10 09:16:07 +0200

    Refactor code

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -37,21 +37,22 @@ public class Controller {
             switch (view.userInput("Enter your command (  start / stop / check / exit  ) : ")) {
                 case ("start"):
                     String name = view.userInput("Enter your name of Timer : ");
-                    if( nameOfTimers.contains(name) ){
+                    if (nameOfTimers.contains(name)) {
                         int index = getIndex(name, listOfTimers);
-                        listOfTimers.get(index).setFlag(true);
-                    }else{
+                        int oldId = listOfTimers.get(index).getId();
+                        listOfTimers.remove(index);
+                        listOfThreads.remove(index);
+
+                        Timer timer = new Timer(name, oldId, true);
+                        idCounter = getIdCounter(listOfTimers, listOfThreads, idCounter, timer);
+
+                    } else {
                         nameOfTimers.add(name);
                         Timer timer = new Timer(name, idCounter, true);
-                        listOfTimers.add(timer);
-                        Thread timerthread = new Thread(timer);
-                        listOfThreads.add(timerthread);
-                        timerthread.start();
-                        idCounter++;
+                        idCounter = getIdCounter(listOfTimers, listOfThreads, idCounter, timer);
                     }
                     break;
                 case ("stop"):
-
                     String timerForStop = view.userInput("Enter name of Timer for STOP : ");
                     int index = getIndex(timerForStop, listOfTimers);
                     listOfTimers.get(index).setFlag(false);
@@ -60,15 +61,16 @@ public class Controller {
                 case ("check"):
 
                     String timerForcheck = view.userInput("Enter name of Timer for check / or tape 'all' : ");
-                    if(timerForcheck.equals("all")){
+                    if (timerForcheck.equals("all")) {
                         view.printTimers(listOfTimers);
-                    }else{
+                    } else {
                         int index2 = getIndex(timerForcheck, listOfTimers);
                         view.printTimer(index2, listOfTimers);
                     }
                     break;
 
                 case ("exit"):
+                    
                     flag = false;
                     System.exit(0);
                     break;
@@ -76,6 +78,15 @@ public class Controller {
                     // code block
             }
         }
+    }
+
+    private int getIdCounter(List<Timer> listOfTimers, List<Thread> listOfThreads, int idCounter, Timer timer) {
+        listOfTimers.add(timer);
+        Thread timerthread = new Thread(timer);
+        listOfThreads.add(timerthread);
+        timerthread.start();
+        idCounter++;
+        return idCounter;
     }
 
 
